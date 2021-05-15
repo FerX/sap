bf53fd236b4cbccc234d7dce472cc5c77b6b28b1
Subbacqueo <raffaele.benedetti@gmail.com>
2021-05-15 23:55:32 +0200

    Update sap.js

--- sap.js
+++ sap.js
@@ -8,7 +8,9 @@
  * 
  * changelog
  * 
- * 1.x aggiunte righe con dati statistici calcolati su un numero di anni selezionabile. Nomi funzioni e dati in inglese.
+ *
+ * 1.x aggiunta possibiulità di selezionare le righe per il calcolo. 
+ * 1.x aggiunte righe con dati statistici calcolati su un numero di anni selezionabile. Nomi funzioni e dati in inglese. 
  * 1.3 agganciatata scritta PRO con il logo, configurazione esterna
  * 
 **/
@@ -17,14 +19,20 @@
 var SAP = {
 	slogan : 'Slogan',
 	showPro: false  
-}
+};
 
 var configSAP = {
 	slogan : 'Slogan',
 	showPro: false  
-}
-*/
+};
+var configBT = {
+	years : 15
+};
+*/	
 
+
+//Backtest calculated data feature
+	
 // sort array ascending
 const asc = arr => arr.sort((a, b) => a - b);
 
@@ -41,45 +49,20 @@ const quantile = (arr, q) => {
 	}
 };
 
-//25° percentile
-const q25 = arr => quantile(arr, .25);
+//BACKTEST FEATURES
+//lower percentile (default 5%)
+const qlow = arr => quantile(arr, .05);
 
-//50° percentile=median
+//BACKTEST FEATURES
+//median (50%)
 const q50 = arr => quantile(arr, .50);
 const median = arr => q50(arr);
 
-//75° percentile
-const q75 = arr => quantile(arr, .75);
+//BACKTEST FEATURES
+//higher percentile (default 95%)
+const qhigh = arr => quantile(arr, .95);
 
-//create  as many rows as they are in the table-array data
-//created rows are inserted at the start of "table" in the document, immediately after the header
-//the content of data is used as column header of the new rows
-//the other cells are left blank.
-//13 cells in total are created per each rows
-function generate_table(table, data) {
-	for (let element of data) {
-
-		//skip the header
-		let row = table.insertRow(1);
-
-		//column header
-		for (key in element) {
-			let cell = row.insertCell();
-			cell.style.textAlign = 'center';
-			cell.style.fontStyle = 'italic';
-			cell.style.fontWeight = 'bold';
-			let text = document.createTextNode(element[key]);
-			cell.appendChild(text);
-		}
-
-		//blank cells
-		for (let step = 0; step < 12; step++) {
-			let cell = row.insertCell();
-			cell.style.textAlign = 'center';
-		}
-	}
-}
-
+//BACKTEST FEATURES
 //write the column array "data" in the column "colonna"  of the table-array "table" passed with header
 function write_table(table, data, colonna) {
 
@@ -93,6 +76,7 @@ function write_table(table, data, colonna) {
 	}
 }
 
+//BACKTEST FEATURES
 //calculate the statistical data of the backtest table
 function calculate_data() {
 
@@ -100,7 +84,7 @@ function calculate_data() {
 	var myTableArray = [];
 
 	//create a column array for calculated data 
-	var calculated_data = new Array(7); //MIN,MAX,AVERAGE,STD_DEV,75°PERC,50°PERC,25°PERC
+	var calculated_data = new Array(7); //MIN,MAX,AVERAGE,STD_DEV,HIGH_PERC,50°PERC,LOW_PERC
 
 	//Reference to the tables in the SA backtest page. 
 	var BT_Tables = document.getElementsByTagName("table");
@@ -108,18 +92,22 @@ function calculate_data() {
 	//The table to be copied is the second one
 	var BT_Table = BT_Tables[1];
 
+	//Get the collection of checkboxes of the rows
+	var BT_check_row = document.getElementsByClassName("check_row");
+
 	//Calculate the number of rows excluding the rows of the calculated data + header (8 total) (this function is executed once the additional rows are already created)
 	var rowLength_max = BT_Table.rows.length-8;
 
 	//Read how many rows of the table the user wnat to analyze
-	var rowLength = parseInt(document.getElementById("myText").value);
-
-	//if the inserted rows are greater than the  existing, the rows are blocked to the maximum 
+	//var rowLength = parseInt(document.getElementById("myText").value);
+	var rowLength=configBT.years;
+	
+	//if the specified rows are greater than the existing, the rows are blocked to the maximum 
 	if (rowLength>rowLength_max) {
 		rowLength=rowLength_max;
 	}
 
-	//copy the backtest table to the data table-array (excluding headers and calculated data   
+	//copy the backtest table to the data table-array (excluding headers and calculated data, row by row)
 	for (var i = 8; i < (rowLength+8); i++){
 
 		//take the collection of the cells of the row   
@@ -128,13 +116,16 @@ function calculate_data() {
 		//Count how many cells there are
 		var cellLength = rowCells.length;
 
-		//copy the row cell-by-cell in a new row array
-		var myTableRow = new Array (cellLength);
-		for(var j = 0; j < cellLength; j++){	
-			myTableRow[j] = rowCells.item(j).innerText;
+		//if the row is selected by the checkbox, copy the row cell-by-cell in a new row array
+		
+		if(BT_check_row[i-8].checked){
+			var myTableRow = new Array (cellLength);
+			for(var j = 0; j < cellLength; j++){	
+				myTableRow[j] = rowCells.item(j).innerText;
+			}
+			//push the copied row in the data table-array
+			myTableArray.push(myTableRow);
 		}
-		//push the copied row in the data table-array
-		myTableArray.push(myTableRow);
 	}
 
 	// process the columns with numeric data
@@ -144,7 +135,8 @@ function calculate_data() {
 
 		//convert the text content of every cell of the column to float
 		var col = myTableArray.map(function(value,index) { return value[c]; }).map(function (x) {return parseFloat(x); });
-
+		
+		
 		//calculate the minimum and round to 2 decimal digit
 		var val_min=Math.min(...col).toFixed(2);
 
@@ -170,14 +162,14 @@ function calculate_data() {
 		val_dev_std=Math.sqrt(val_dev_std);
 		val_dev_std=parseFloat(val_dev_std).toFixed(2);
 
-		//75mo percentile 
-		var val_75moperc=q75(col).toFixed(2);
+		//higher percentile 
+		var val_high_perc=qhigh(col).toFixed(2);
 
-		//50mo percentile (MEDIAN)
-		var val_50moperc=q50(col).toFixed(2);
+		//median (50 percentile)
+		var val_median=q50(col).toFixed(2);
 
-		//25mo percentile 
-		var val_25moperc=q25(col).toFixed(2);
+		//lower percentile 
+		var val_low_perc=qlow(col).toFixed(2);
 
 		//prepare the column array with calculated data 
 		let calculated_data = [
@@ -185,9 +177,9 @@ function calculate_data() {
 		{ data: val_max},
 		{ data: val_med},
 		{ data: val_dev_std},
-		{ data: val_75moperc},
-		{ data: val_50moperc},
-		{ data: val_25moperc}
+		{ data: val_high_perc},
+		{ data: val_median},
+		{ data: val_low_perc}
 		];
 		
 		//write the column in the table of the page
@@ -241,8 +233,25 @@ function calculate_data() {
 	}	
 
 	//write the message of the calculated data
-	document.getElementById("anni").innerHTML = "Sono stati processati " + (rowLength) +" anni.";
+	//document.getElementById("anni").innerHTML = "Sono stati processati " + (rowLength) +" anni.";
 
+}	
+
+function set_data(yy)
+{
+	
+	//Get the collection of checkboxes of the rows
+	var BT_check_row = document.getElementsByClassName("check_row");
+	
+	i=0;
+	for (c of BT_check_row)
+	{
+		c.checked=false;
+		if ((i)<yy) {
+			c.checked=true;
+		}
+		i++;
+	}
 }
 
 function superSeasonAlgo($){
@@ -261,7 +270,6 @@ function superSeasonAlgo($){
 		config.showPro = configSAP.showPro
 	}
 	
-
 	const url = window.location.pathname
 	const urlWithPopUp = ["/system/strategies/","/system/strategies/recommendation","/system/strategies/scanner"]
 
@@ -286,7 +294,6 @@ function superSeasonAlgo($){
 		})
 	}
 
-	
 	function waitForElement(selector,cb){
 		if ($(selector).length) {
           return cb();
@@ -307,7 +314,6 @@ function superSeasonAlgo($){
 	    }, 100);
   	}
   	
-
 	function injectDataInPopUp(){
 		var teste = $("thead .grid-label th");
 	    var el = $("tr.info");
@@ -363,11 +369,67 @@ function superSeasonAlgo($){
 
 	}
 	
-	//Backtest calculated data feature
+	//BACKTEST FEATURES
 	function injectDataInBackTest(){
+		
+		
+		//BACKTEST FEATURES
+		//create  as many rows as they are in the table-array data
+		//created rows are inserted at the start of "table" in the document, immediately after the header
+		//the content of data is used as column header of the new rows
+		//the other cells are left blank.
+		//13 cells in total are created per each rows
+		function generate_table(table, data) {
+			
+			//insert the rows for calculated data
+			for (let element of data) {
+		
+				//skip the header
+				let row = table.insertRow(1);
+		
+				//column header
+				for (key in element) {
+					let cell = row.insertCell();
+					cell.style.textAlign = 'center';
+					cell.style.fontStyle = 'italic';
+					cell.style.fontWeight = 'bold';
+					let text = document.createTextNode(element[key]);
+					cell.appendChild(text);
+				}
+		
+				//blank cells
+				for (let step = 0; step < 12; step++) {
+					let cell = row.insertCell();
+					cell.style.textAlign = 'center';
+				}
+				
+			}
+			
+			//add a checkbox for each row of the table to select the rows to be processed
+			for (step = 8;step < table.rows.length; step++){
+				
+				var tr = table.rows[step];
+				var td = document.createElement('td');
+		    	var bt_checkbox = document.createElement("INPUT");
+			    bt_checkbox.setAttribute("type", "checkbox");
+			    bt_checkbox.setAttribute("class", "check_row");
+			    
+			    if(step-8<configBT.years){
+			    	bt_checkbox.setAttribute("checked", "true");	
+			    }
+			    
+			    td.appendChild(bt_checkbox);
+				tr.appendChild(td);
+		
+			}
+			
+			
+		}
 		
 		//create a textbox to specify the amount of data to use for calculation
 		//clicking the button, the function to calulate the statistical data is invoked
+		
+		/*
 		var html='<div class="row SSA_BT_Input" style="text-align:center;">';
 			html+='<p>Specifica gli anni su cui fare i calcoli</p>'
 			html+='<input type="text" id="myText" value="15">';
@@ -375,6 +437,22 @@ function superSeasonAlgo($){
 			html+='<p id="anni"></p>'
 			html+='<p>RB1.10</p>'
 			html+='</div>';
+		*/
+		
+		var html='<div class="row SSA_BT_Input" style="text-align:center;">';
+			html+='<p>Specifica gli anni su cui fare i calcoli</p>'
+			html+='<p>'
+			html+='<button onclick="set_data(5)">Ultimi 5</button>'
+			html+='<button onclick="set_data(10)">Ultimi 10</button>'
+			html+='<button onclick="set_data(15)">Ultimi 15</button>'
+			html+='<button onclick="set_data(99)">Tutti</button>'
+			html+='<button onclick="set_data(0)">Pulisci</button>'
+			html+='</p>'
+			html+='<p></p>'
+			html+='<p><button onclick="calculate_data()">Aggiorna</button></p>'
+			html+='<p>RB1.10</p>'
+			html+='</div>';
+		
 
 		//select the backtest chart as a refernce in the DOM
 		var BTchart = document.getElementById("snippet--backtest-chart");
@@ -384,16 +462,15 @@ function superSeasonAlgo($){
 
 		//prepare the header column of the table for calculated data
 		let calculated_data_header= [
-			{ name: "25°perc"},
+			{ name: "5°perc"},
 			{ name: "MEDIANA"},
-			{ name: "75°perc"},
+			{ name: "95°perc"},
 			{ name: "DEV_STD"},
 			{ name: "MED"},
 			{ name: "MAX"},
 			{ name: "MIN"}
 		];
 
-		
 		//Reference to the tables in the SA backtest page. 
 		var BT_Tables = document.getElementsByTagName("table");
 
@@ -428,4 +505,5 @@ css.type = "text/css"
 css.href = 'https://cdn.jsdelivr.net/gh/FerX/sap@main/sap.css'
 
 document.getElementsByTagName('head')[0].appendChild(css);
+
 
